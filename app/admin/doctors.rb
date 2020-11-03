80760eda4f2f44ed2489ce01ea366c991402d69e
martin kiwala <martinkiwala4@gmail.com>
2020-11-03 12:09:37 +0300

    changes in views and doctor structure in active admin

--- app/admin/doctors.rb
+++ app/admin/doctors.rb
@@ -1,18 +1,31 @@
 ActiveAdmin.register Doctor do
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
   permit_params :email, :password, :password_confirmation, :username, :role
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :username, :locked_at, :role]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
 
+    index do
+      selectable_column
+      id_column
+      column :email
+      column :username
+      column :created_at
+      column :role
+      actions
+    end
+
+    filter :email
+    filter :username
+    filter :current_sign_in_at
+    filter :sign_in_count
+    filter :created_at
+
+    form do |f|
+      f.inputs "Doctor Details" do
+          f.input :email
+          f.input :username
+          f.input :password
+          f.input :password_confirmation, label: 'Password Confirmation'
+          f.input :role
+      end
+      f.actions
+    end
 end
