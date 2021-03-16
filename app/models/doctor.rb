e0aeaac0cb11b5126ae22836d2139fa560b5cda2
imutumba <imutumba@skylinesms.com>
2021-03-16 14:19:23 +0300

    removed locable

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,6 +1,6 @@
 class Doctor < ApplicationRecord
   attr_writer :login
-  devise :database_authenticatable, :rememberable, :validatable, :lockable, :timeoutable, authentication_keys: [:username]
+  devise :database_authenticatable, :rememberable, :validatable, :timeoutable, authentication_keys: [:username]
 
   validates :email, :username, presence: true
   validates :email, :username, uniqueness: true
