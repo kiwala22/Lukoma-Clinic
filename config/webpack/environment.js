68bd4cb4d310d063ce3b2d2a6e6dd52db16a2480
martin kiwala <martinkiwala4@gmail.com>
2021-03-18 13:42:42 +0300

    removed source maps from production environment

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,7 +2,4 @@ const { environment } = require('@rails/webpacker')
 const jquery = require('./plugins/jquery')
 
 environment.plugins.prepend('jquery', jquery)
-
-const devtool = process.env.DEVTOOL;
-if (devtool) environment.config.merge({ devtool });
 module.exports = environment
