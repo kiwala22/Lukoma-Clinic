5991b5bd5f961dedc75ca7e854d7bba078e59d10
martin kiwala <martinkiwala4@gmail.com>
2021-03-18 13:34:39 +0300

    change in webpacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,4 +2,7 @@ const { environment } = require('@rails/webpacker')
 const jquery = require('./plugins/jquery')
 
 environment.plugins.prepend('jquery', jquery)
+
+const devtool = process.env.DEVTOOL;
+if (devtool) environment.config.merge({ devtool });
 module.exports = environment
