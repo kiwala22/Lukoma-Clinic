d85231bfab7153227e28fea8b16e188b5560d274
martin kiwala <martinkiwala4@gmail.com>
2021-04-07 13:31:46 +0300

    change for webpacker

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,6 +1,7 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
+environment.splitChunks()
 
 const config = environment.toWebpackConfig()
 config.devtool = 'none'
