db222bc6c63c200d05a64a11356cbc53a904b51e
martin kiwala <martinkiwala4@gmail.com>
2021-06-07 16:38:24 +0300

    specifying ssh port

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,24 @@ set :repo_url, "git@bitbucket.org:kiwala22/lukoma-clinic.git"
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/var/www/html/lukoma-clinic"
 
+
+
+# Default value for :scm is :git
+set :scm, :git
+
+set :ssh_options, {:forward_agent => true, port: 8181}
+
+set :deploy_via, :copy
+
+# Default value for :format is :pretty
+set :format, :pretty
+
+# Default value for :log_level is :debug
+set :log_level, :debug
+
+# Default value for :pty is false
+set :pty, true
+
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
